fc4c4b7ed4dbb748b379c051666fe838be9dfcd3
Bertrand Giot <379959+bgiot@users.noreply.github.com>
2019-11-25 13:36:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 #
 # This script should be run via:
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/juliantellez/up/master/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/bgiot/up/bgiot/minimal/install.sh)"
 #
 
 mkdir -p ~/.up; cd ~/.up; \
-curl -L https://github.com/juliantellez/up/tarball/master | tar -xzv --strip-components 1 &> /dev/null; \
+curl -L https://github.com/bgiot/up/tarball/bgiot/minimal | tar -xzv --strip-components 1 &> /dev/null; \
 sh bootstrap.sh
